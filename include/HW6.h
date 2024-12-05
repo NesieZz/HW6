e78793d08721aeeb176239507f61e8f7547a97c0
NesieZz <tyhhyhh123@gmail.com>
2024-12-05 19:40:05 +0800

    加入專案檔案。

---
+++ include/HW6.h
@@ -0,0 +1,3 @@
+#pragma once
+int HW6_1();
+int HW6_2();
---
+++ source/Main.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "../include/HW6.h"
+
+int main(void)
+{
+	HW6_1();
+	HW6_2();
+	system("pause");
+}
